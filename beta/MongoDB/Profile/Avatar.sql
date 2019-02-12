8e166862d2a0d25c4019d9e21938b59a9959fb16
Aingty Eung <aingtyeung@yahoo.com>
2019-02-11 22:17:54 -0800

    Created Avatar's Table Populated 4 data into the table.

---
+++ beta/MongoDB/Profile/Avatar.sql
@@ -0,0 +1,62 @@
+-- Creating the Collection called "Avatar"
+db.createCollection("Avatar");
+
+-- "Avatar" Model
+Avatar:
+{
+    avatarUID   :   int, 
+    avatarName  :   String, 
+    hairStyle   :   int,
+    hairColor   :   int,
+    eyeColor    :   int,
+    furColor    :   int,
+    animal      :   int,
+    attribute   :   Map<String, int>
+}
+
+-- Populating "Avatar" Collection
+db.Avatar.insertMany
+(
+    [
+        {
+            avatarUID   :   1, 
+            avatarName  :   "Panda", 
+            hairStyle   :   2,
+            hairColor   :   3,
+            eyeColor    :   2,
+            furColor    :   1,
+            animal      :   1,
+            attribute   :   Map<String, int>
+        },
+        {
+            avatarUID   :   2, 
+            avatarName  :   "Asian", 
+            hairStyle   :   3,
+            hairColor   :   3,
+            eyeColor    :   2,
+            furColor    :   2,
+            animal      :   2,
+            attribute   :   Map<String, int>
+        },
+        {
+            avatarUID   :   3, 
+            avatarName  :   "Party", 
+            hairStyle   :   4,
+            hairColor   :   5,
+            eyeColor    :   3,
+            furColor    :   2,
+            animal      :   3,
+            attribute   :   Map<String, int>
+        },
+        {
+            avatarUID   :   4, 
+            avatarName  :   "Famous", 
+            hairStyle   :   1,
+            hairColor   :   1,
+            eyeColor    :   1,
+            furColor    :   1,
+            animal      :   1,
+            attribute   :   Map<String, int>
+        }
+    ]
+);

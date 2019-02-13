f0da7990965e30e6f93a55476161dfdda755f17a
Aingty Eung <aingtyeung@yahoo.com>
2019-02-12 16:59:13 -0800

    Created Gear Table Populated with 6 data.

--- beta/MongoDB/Profile/Gear.sql
+++ beta/MongoDB/Profile/Gear.sql
@@ -5,6 +5,90 @@ db.createCollection("Gear");
 Gear:
 {
     gearUID             :   int, 
+    gearName            :   String,
     attributeModifier   :   Map<String, int>,
     spriteID            :   int
 }
+
+-- Populating "Gear" Collection
+db.Gear.insertMany(
+[
+    {
+        gearUID             :   1,
+        gearName            :   "Dev's Helm",
+        attributeModifier   : 
+        {
+            Health      :   100,
+            Strength    :   100,
+            Stamina     :   100,
+            Agility     :   100,
+            Intellect   :   100,
+            Dexterity   :   100
+        }
+    },
+    {
+        gearUID             :   2,
+        gearName            :   "Dev's Body",
+        attributeModifier   : 
+        {
+            Health      :   100,
+            Strength    :   100,
+            Stamina     :   100,
+            Agility     :   100,
+            Intellect   :   100,
+            Dexterity   :   100
+        }
+    },
+    {
+        gearUID             :   3,
+        gearName            :   "Dev's Pant",
+        attributeModifier   : 
+        {
+            Health      :   100,
+            Strength    :   100,
+            Stamina     :   100,
+            Agility     :   100,
+            Intellect   :   100,
+            Dexterity   :   100
+        }
+    },
+    {
+        gearUID             :   4,
+        gearName            :   "Dev's Boot",
+        attributeModifier   : 
+        {
+            Health      :   100,
+            Strength    :   100,
+            Stamina     :   100,
+            Agility     :   100,
+            Intellect   :   100,
+            Dexterity   :   100
+        }
+    },
+    {
+        gearUID             :   5,
+        gearName            :   "Dev's Glove",
+        attributeModifier   : 
+        {
+            Health      :   100,
+            Strength    :   100,
+            Stamina     :   100,
+            Agility     :   100,
+            Intellect   :   100,
+            Dexterity   :   100
+        }
+    },
+    {
+        gearUID             :   6,
+        gearName            :   "Dev's Weapon",
+        attributeModifier   : 
+        {
+            Health      :   100,
+            Strength    :   100,
+            Stamina     :   100,
+            Agility     :   100,
+            Intellect   :   100,
+            Dexterity   :   100
+        }
+    }
+]);

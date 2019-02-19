2e712f1f209dfc587e208d60b96c879ca0ca22b4
Aingty Eung <aingtyeung@yahoo.com>
2019-02-19 13:21:06 -0800

    Finished Group Table Added 3 data entries.

--- beta/MongoDB/Group/Group.sql
+++ beta/MongoDB/Group/Group.sql
@@ -4,17 +4,28 @@ db.createCollection("Group");
 -- "Group" Model
 Group:
 {
-    GroupUID     :   int, 
-    GroupName    :   String, 
-    password    :   String,
-    firstName   :   String,
-    lastName    :   String,
-    email       :   String,
-    phone       :   String,
-    dateOfBirth :   String
+    groupUID    :   int,
+    groupName   :   String,
+    groupCode   :   String
+
 }
 
 -- Populating "Group" Collection
 db.Group.insertMany(
 [
     {
+        groupUID    :   1,
+        groupName   :   "Food-Guild",
+        groupCode   :   "123456"
+    },
+    {
+        groupUID    :   2,
+        groupName   :   "Boba4Life",
+        groupCode   :   "123456"
+    },
+    {
+        groupUID    :   3,
+        groupName   :   "Sweettooth",
+        groupCode   :   "123456"
+    }
+]);
